7eae6200b02c41567b91ed03824c9e52c995daba
jdptrick <45019245+jdptrick@users.noreply.github.com>
2022-07-07 09:06:19 -0400

    Add files via upload

--- Check_User_Logon.ps1
+++ Check_User_Logon.ps1
@@ -1,6 +1,6 @@
 Write-Host "`r`n[+] Terminal Services Logons: `r`n"
 Write-Host "+++++++++++++++++++++++++++++++++++++++++"
-$Before = Get-Date 2022/7/1
-$After = Get-Date 2022/7/3
-Get-WinEvent -FilterHashtable @{ LogName='Security'; StartTime=$After; EndTime=$Before; Id='4624'} | Where-Object {$_.Message -match "Logon Type:\s+10"} | Select-Object TimeCreated,Message
+$Before = Get-Date 2022/07/08
+$After = Get-Date 2022/07/06
+Get-WinEvent -FilterHashtable @{ LogName='Security'; StartTime=$After; EndTime=$Before; Id='4624'} | where {$_.Message -match "Logon Type:\s+10"} | Select-Object TimeCreated,Message
 
