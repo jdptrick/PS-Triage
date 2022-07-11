187f9da59693cdfb73c42046471b51bf4265947b
jdptrick <45019245+jdptrick@users.noreply.github.com>
2022-07-11 10:42:51 -0400

    Delete Check_User_Logon.ps1

--- Check_User_Logon.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "`r`n[+] Terminal Services Logons: `r`n"
-Write-Host "+++++++++++++++++++++++++++++++++++++++++"
-$Before = Get-Date 2022/07/08
-$After = Get-Date 2022/07/06
-Get-WinEvent -FilterHashtable @{ LogName='Security'; StartTime=$After; EndTime=$Before; Id='4624'} | where {$_.Message -match "Logon Type:\s+10"} | Select-Object TimeCreated,Message
-
