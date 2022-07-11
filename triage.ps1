7555accab1b0d6b5a90a63698765aba9b7d9c3ad
jdptrick <45019245+jdptrick@users.noreply.github.com>
2022-07-11 10:14:45 -0400

    Add files via upload

--- triage.ps1
+++ triage.ps1
@@ -19,17 +19,11 @@
     # Get-Item $file -stream * | ? stream -NE ':$Data'
 # }
 
-# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Check logons via terminal services (RDP, WMI, PSExec)
-
-Write-Host "`r`n[+] Terminal Services Logons: `r`n"
-Write-Host "+++++++++++++++++++++++++++++++++++++++++"
-$After = Get-Date 2022/7/6
-$Before = Get-Date 2022/7/8
-Get-WinEvent -FilterHashtable @{ LogName='Security'; StartTime=$After; EndTime=$Before; Id='4624'} | Where-Object {$_.Message -match "Logon Type:\s+10"} | select TimeCreated,Message
-
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Check Scheduled Tasks
+
+Write-Host "`r`n[+] Scheduled Tasks: `r`n"
+Write-Host "+++++++++++++++++++++++++++++++++++++++++"
 
 $tasks = Get-ChildItem "C:\Windows\System32\Tasks" -Recurse
 
@@ -41,10 +35,17 @@ ForEach($task in $tasks){
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Check Services
+
+Write-Host "`r`n[+] Services: `r`n"
+Write-Host "+++++++++++++++++++++++++++++++++++++++++"
+
 Get-WmiObject win32_service | select Name, DisplayName | Format-List
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Check Run/RunOnce registry keys for Machine and User
+
+Write-Host "`r`n[+] Run/RunOnce Registry Keys `r`n"
+Write-Host "+++++++++++++++++++++++++++++++++++++++++"
 
 #Define array with registry values (machine keys)
 $sysKeys = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run", "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce"
@@ -76,3 +77,13 @@ Get-ItemProperty Registry::\hku\*\software\microsoft\windows\currentversion\runo
 ForEach($key in $sysKeys){
     Get-ItemProperty Registry::$key
 }
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Check logons via terminal services (RDP, WMI, PSExec)
+
+Write-Host "`r`n[+] Terminal Services Logons: `r`n"
+Write-Host "+++++++++++++++++++++++++++++++++++++++++"
+
+$After = Get-Date 2022/7/6
+$Before = Get-Date 2022/7/8
+Get-WinEvent -FilterHashtable @{ LogName='Security'; StartTime=$After; EndTime=$Before; Id='4624'} | Where-Object {$_.Message -match 'Logon Type:		(3|10)'} 
